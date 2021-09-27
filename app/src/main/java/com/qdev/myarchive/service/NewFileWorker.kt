d667b4cc09ea4fddb03a532b760ec95d5d8bd788
Kudashov Vladimir <V.kudashov.83@mail.ru>
2021-09-27 11:00:26 +0300

    Update android tools. NewFileWorker debug and test.

--- app/src/main/java/com/qdev/myarchive/service/NewFileWorker.kt
+++ app/src/main/java/com/qdev/myarchive/service/NewFileWorker.kt
@@ -15,9 +15,9 @@ class NewFileWorker(context: Context, workerParams: WorkerParameters) :
     Worker(context, workerParams) {
 
     companion object {
-        val tag: String = javaClass.simpleName
+        const val tag: String =  "NewFileWorker"
 
-        val pathToWatch: File
+        val pathForObserve: File
         get () {
             //TODO file from file system and request to file access
             return File ("./MyFiles")
@@ -26,24 +26,14 @@ class NewFileWorker(context: Context, workerParams: WorkerParameters) :
 
     override fun doWork(): Result {
         try {
-            Log.d(tag, "Worker start")
-//            val file = applicationContext.getExternalFilesDir()
-            val observer = object : FileObserver(pathToWatch) {
-                override fun onEvent(event: Int, file: String?) {
-                    Log.d(tag, "File " + pathToWatch.toString()
-                            + file.toString() + ", event: $event")
-                    Toast.makeText(applicationContext, "$file observed!",
-                        Toast.LENGTH_LONG).show()
-                }
-            }
-            observer.startWatching()
-            Thread.sleep(500)
-            return Result.retry();
+            Log.d(tag, "Worker start (5000 ms)")
+            Thread.sleep(5000)
+            Log.d(tag, "Worker stop")
         } catch (ex: Exception) {
             Log.d(tag, "Error ${ex.printStackTrace()}")
-            return Result.failure(); //или Result.retry()
+            return Result.failure()
         }
-        return Result.success()
+        return Result.retry()
     }
 
 }
